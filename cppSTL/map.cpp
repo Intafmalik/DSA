a53e3a32b22363bccab0e749232c0162990cf024
Intaf Malik <137301879+Intafmalik@users.noreply.github.com>
2024-10-30 16:59:11 +0000

    mapCommit

--- cppSTL/map.cpp
+++ cppSTL/map.cpp
@@ -1,19 +1,18 @@
-#include<iostream>
-#include<map>
+#include <iostream>
+#include <map>
 using namespace std;
 
-int main(){
-    map<string,int> mp;
-    mp["TV"]=100;
-    mp["watch"]=200;
-    mp["computer"]=300;
-    mp["laptop"]=100;
+int main()
+{
+    map<string, int> mp;
+    mp["TV"] = 100;
+    mp["watch"] = 200;
+    mp["computer"] = 300;
+    mp["laptop"] = 100;
 
-    for(auto  i:  mp){
-   cout<<i.first <<" "<<i.second<<endl;
-   
+    for (auto i : mp)
+    {
+        cout << i.first << " " << i.second << endl;
     }
     return 0;
-
-
 }
