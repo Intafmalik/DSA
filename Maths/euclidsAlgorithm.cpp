ed538807535d3cd1d24fa2ae632ba94937a2c296
Intaf Malik <137301879+Intafmalik@users.noreply.github.com>
2024-11-10 04:52:31 +0000

    recursion function add

--- Maths/euclidsAlgorithm.cpp
+++ Maths/euclidsAlgorithm.cpp
@@ -1,30 +1,51 @@
 // Eulids Algorithm is used to find the greatest common diviser (GCD) or HCF
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int gcd(int n1, int n2){
-    while(n1!=0 && n2!=0){
-        if(n1>n2){
-            n1= n1%n2;
-        }else{
-            n2= n2%n1;
+int gcd(int n1, int n2)
+{
+    while (n1 != 0 && n2 != 0)
+    {
+        if (n1 > n2)
+        {
+            n1 = n1 % n2;
+        }
+        else
+        {
+            n2 = n2 % n1;
         }
     }
-    if(n1==n2){
-        return n1;
-    }else if(n1==0){
-        return n2;
-    }else if(n2==0){
+    if (n1 == n2)
+    {
         return n1;
     }
-
+    else if (n1 == 0)
+    {
+        return n2;
+    }
+    else if (n2 == 0)
+    {
+        return n1;
+    }
 }
 
-int main(){
-    int n1,n2;
-    cout<<"Enter any 2 number to find HCF: ";
-    cin>>n1>>n2;
-    cout<<gcd(n1,n2)<<" is the greatest common diviser "<<endl;
+
+//  using recursion function to find gcd
+
+int recGCD(int a, int b){
+    if(b==0)return a;
+   return recGCD(b, a%b);
+}
+int main()
+{
+    int n1, n2;
+    cout << "Enter any 2 number to find HCF: ";
+    cin >> n1 >> n2;
+    cout << gcd(n1, n2) << " is the greatest common diviser " << endl;
+
+    cout<<"Using Resrcion Function: "<<endl;
+    cout << recGCD(n1, n2) << " is the greatest common diviser " << endl;
+
     return 0;
 }
